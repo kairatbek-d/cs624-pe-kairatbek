544cc95f84f10bf5de4e84a37571a6e7dc78ded1
KaIRaTBeK <45318061+kairatbek-d@users.noreply.github.com>
2023-10-23 16:39:05 -0700

    PE03 - Part 1

---
+++ PE03-todos/cs624-fall-2023-hos03-kairatbek-d-main/todos/App.js
@@ -0,0 +1,68 @@
+import React, { Component } from 'react';
+import { ScrollView, StyleSheet, Text, View } from 'react-native';
+import Heading from './Heading';
+import Input from './Input';
+import Button from './Button';
+
+let todoIndex = 0
+
+class App extends Component {
+
+  constructor() {
+    super()
+    this.state = {
+      inputValue: '',
+      todos: [],
+      type: 'All'
+    }
+    this.submitTodo = this.submitTodo.bind(this)
+  }
+
+  inputChange(inputValue) {
+    console.log(' Input Value: ' , inputValue)
+    this.setState({ inputValue })
+  }
+
+  submitTodo () {
+    if (this.state.inputValue.match(/^\s*$/)) {
+      return
+    }
+    const todo = {
+      title: this.state.inputValue,
+      todoIndex,
+      complete: false
+    }
+    todoIndex++
+    const todos = [...this.state.todos, todo]
+    this.setState({ todos, inputValue: '' }, () => {
+      console.log('State: ', this.state)
+    }) 
+  }
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <ScrollView keyboardShouldPersistTaps='always' style={styles.content}>
+          <Heading />
+          <Input 
+            inputValue={this.state.inputValue}
+            inputChange={(text) => this.inputChange(text)} />
+          <Button submitTodo={this.submitTodo} />
+        </ScrollView>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#f5f5f5',
+  },
+  content: {
+    flex: 1,
+    paddingTop: 60
+  }
+});
+
+export default App
